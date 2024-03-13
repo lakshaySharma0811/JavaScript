620258fc4d11307146c4e21eb92e6c33d3074668
Lakshay <151620943+lakshaySharma0811@users.noreply.github.com>
2024-03-13 20:07:46 +0530

    Create first.js

---
+++ first.js
@@ -0,0 +1,44 @@
+console.log("Welcome to lovely professional university");
+console.log("My name is lakshay");
+console.log("Hello");
+
+
+
+fullName = "Lakshay Sharma";
+FULLNAME = "LAKSHAY SHARMA";
+fullName123 = "LPU";
+_fullName = "Rohit Sharma";
+$fullName = "Virat Kohli";
+FULLName = "Keshav Kumar";
+Console = "Karn";
+age = 24;
+price = 96.5;
+x = null;
+y = undefined;
+isFollow = false;
+
+console.log(fullName);
+console.log(FULLNAME);
+console.log(fullName123);
+console.log(_fullName);
+console.log($fullName);
+console.log(FULLName);
+console.log(Console);
+console.log(age);
+console.log(price);
+console.log(x);
+console.log(y);
+console.log(isFollow);
+
+
+
+let fullName = "Tony Stark";
+let age = 20;
+let totalPrice = 1000;
+
+console.log(fullName);
+console.log(age);
+console.log(totalPrice);
+
+
+
